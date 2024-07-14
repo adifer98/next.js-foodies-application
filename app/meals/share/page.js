cf2996b5f488df6ea53a638dfdf21ea62aced266
adifer98 <104688832+adifer98@users.noreply.github.com>
2024-07-14 17:03:08 +0300

    continued

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,14 @@
+'use client';
+
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
 import {shareMeal} from "@/lib/actions";
+import MealFormSubmit from "@/components/meals/meal-form-submit";
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
 
+  const [state, formAction] = useFormState(shareMeal, {message: null});
 
   return (
     <>
@@ -14,7 +19,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,8 +48,9 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label='Your image' name='image'/>
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+           <MealFormSubmit />
           </p>
         </form>
       </main>
